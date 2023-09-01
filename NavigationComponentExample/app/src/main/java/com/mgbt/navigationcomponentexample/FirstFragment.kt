35d1326436da3fa3f19d56be2724001d7fada484
Matías Gabriel Blanco Toledo <blancotoledomatiasgabriel@gmail.com>
2023-09-01 15:34:40 -0300

    Added seventh example application (Demo of how to use NavigationComponent)

---
+++ NavigationComponentExample/app/src/main/java/com/mgbt/navigationcomponentexample/FirstFragment.kt
@@ -0,0 +1,25 @@
+package com.mgbt.navigationcomponentexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_first, container, false)
+        val btnNavigate = root.findViewById<Button>(R.id.btnNavigate)
+        btnNavigate.setOnClickListener {
+            findNavController().navigate(FirstFragmentDirections.actionFirstFragmentToSecondFragment(
+                name = "Matías")
+            )
+        }
+        return root
+    }
+}
